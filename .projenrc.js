c6b93c2e0894c83a0dca17b9ae000f7671028aaf
Mengxin Zhu <mengxinz@amazon.com>
2021-05-19 02:23:01 +0000

    chore: migrate to use projen to manage project

---
+++ .projenrc.js
@@ -0,0 +1,63 @@
+const { AwsCdkTypeScriptApp } = require('projen');
+const project = new AwsCdkTypeScriptApp({
+  cdkVersion: '1.104.0',
+  defaultReleaseBranch: 'master',
+  name: 'sonatype-nexus3',
+  appEntrypoint: 'sonatype-nexus3.ts',
+  cdkDependencies: [
+    '@aws-cdk/aws-certificatemanager',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-efs',
+    '@aws-cdk/aws-eks',
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/aws-lambda-python',
+    '@aws-cdk/aws-logs',
+    '@aws-cdk/aws-s3',
+    '@aws-cdk/aws-route53',
+    '@aws-cdk/lambda-layer-kubectl',
+    '@aws-cdk/cloud-assembly-schema',
+    '@aws-cdk/cx-api',
+  ], /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
+  deps: [
+    'js-yaml@^3.14.1',
+    'sync-request@^6.1.0',
+  ], /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  devDeps: [
+    'lodash@>=4.17.21',
+  ], /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // releaseWorkflow: undefined,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
+  pullRequestTemplate: true /* Include a GitHub pull request template. */,
+  pullRequestTemplateContents: [
+    '*Issue #, if available:*',
+    '',
+    '*Description of changes:*',
+    '',
+    '',
+    'By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.',
+  ],
+  license: 'MIT-0' /* License's SPDX identifier. */,
+  licensed: false /* Indicates if a license should be added. */,
+  gitignore: [
+    '.idea/',
+    '.vscode/',
+    'cdk.context.json',
+    '.DS_Store',
+  ],
+  keywords: [
+    'aws',
+    'sonatype',
+    'nexus3',
+    'aws-cdk',
+    'aws-eks',
+    'eks',
+  ],
+});
+// tricky to override the default synth task
+project.tasks._tasks.synth._steps[0] = {
+  exec: 'cdk synth -c createNewVpc=true',
+};
+project.synth();
--- src/sonatype-nexus3.ts
+++ src/sonatype-nexus3.ts
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { SonatypeNexus3Stack } from '../lib/sonatype-nexus3-stack';
+import { SonatypeNexus3Stack } from './lib/sonatype-nexus3-stack';
 
 const app = new cdk.App();
 const env = app.node.tryGetContext('createNewVpc') ? undefined : {
-    region: process.env.CDK_DEFAULT_REGION,
-    account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+  account: process.env.CDK_DEFAULT_ACCOUNT,
 };
 new SonatypeNexus3Stack(app, 'SonatypeNexus3OnEKS', {
-    env: env,
+  env: env,
 });
 
 cdk.Tags.of(app).add('app', 'nexus3');
