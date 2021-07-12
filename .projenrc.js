db8b55584da50f08b28a42335366e7b67e643b14
Mengxin Zhu <mengxinz@amazon.com>
2021-07-12 07:30:48 +0000

    chore: bump cdk to 1.112.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.108.0',
+  cdkVersion: '1.112.0',
   defaultReleaseBranch: 'master',
   name: 'sonatype-nexus3',
   appEntrypoint: 'sonatype-nexus3.ts',
